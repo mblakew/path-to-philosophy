72add62767cf945f73dc7840b0a07de105a2972a
Blake Williams <michaelbwilliams100@gmail.com>
2020-11-11 16:51:11 -0500

    Certain pages cause max connection errors

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
 import wikipediaapi as wa
 
 # graph search problem
-# using depth first search
 
 en_wiki = wa.Wikipedia("en")
 
 
-def is_valid_request(res):
-    print(res.status_code)
-    if res.status_code == 200:
+def is_valid_request(response):
+    print(response.status_code)
+    if response.status_code == 200:
         return True
     else:
         return False
@@ -19,15 +18,18 @@ def is_valid_request(res):
 # expects url in form /wiki/someTextHere
 def is_valid_url(url):
     # print("url: " + url + "\nurl[:6]: " + url[:6])
-    return (url is not None) and (len(url) > 6) and (url[0] != '#') and (url[:6] == "/wiki/")
+    return url is not None and len(url) > 6 and url[0] != "#" and url[:6] == "/wiki/"
 
 
 def print_url(url):
     print("Current page: " + url + "\n")
 
 
-next_link = input("Enter wiki url in form \"/wiki/PageTitle\" without surrounding quotes: ")
+next_link = input("Enter wiki title to crawl from (e.g. to crawl from "
+                  "\"https://en.wikipedia.org/wiki/Fizz_buzz\", enter \"Fizz_Buzz\" : ")
+next_link = "/wiki/" + next_link
 visited_links = []
+
 # if title is not form /wiki/validTitle break
 if not is_valid_url(next_link):
     print("Invalid title: " + next_link)
@@ -36,25 +38,28 @@ if not is_valid_url(next_link):
 while True:
     next_link = "https://en.wikipedia.org" + next_link
     # make http request with full link
-    req = urllib.request.Request(next_link)
-    content = urllib.request.urlopen(req).read()
-    # if response status code is not 200 break
-    # if not is_valid_request(response):
-    #     print("Invalid response, status code: " + str(response.status_code))
-    #     break
-    # get content of http response
-    # get beautiful soup html
+    # req = urllib.request.Request(next_link)
+    # content = urllib.request.urlopen(req).read()
+    #                                     try:
+    #                                         res = requests.get(x)
+    #                                     except requests.exceptions.ConnectionError:
+    #                                         res.status_code = "Connection refused"
+    #                                         print("Failed because connection was refused")
+    res = requests.get(next_link)
+    content = res.content
+
     soup = BeautifulSoup(content, 'html.parser')
     # get the title from current header
-    title = soup.findAll('h1', {'class': 'firstHeading'})[0].contents[0]
-    if title == "Philosophy":
-        print("We win! Current title is: " + title)
-        exit()
-    else:
-        print("Current title issss: " + title)
+    title = soup.find(id="firstHeading")
+    # print(title)
 
-    content = soup.findAll("div", {"class": "mw-parser-output"})[0]
-    paragraphs = content.findAll('p')
+    print(title.string)
+    if title.string == "Philosophy":
+        print("You crawled to Philosophy!")
+        exit()
+
+    paragraphs = soup.find(id="bodyContent").findAll("p")
+    # paragraphs = content.findAll('p')
 
     found_next_url = False
     for p in paragraphs:
@@ -71,49 +76,12 @@ while True:
                     if not parenthesis:
                         if not isinstance(text, str):
                             try:
-                                x = text['href']
-                                if not x in visited_links:
+                                link = text['href']
+                                # print("X: " + x)
+                                if link not in visited_links:
                                     found_next_url = True
-                                    next_link = x
+                                    # print(x)
+                                    next_link = link
                                     visited_links.append(next_link)
                             except:
                                 pass
-
-
-    # next_url_found = False
-    # for paragraph in paragraphs:
-    #     if not next_url_found:
-    #         parenthesis_in_progress = False
-    #         for thing in paragraph.contents:
-    #             if not next_url_found:
-    #                 # print(thing)
-    #                 if "(" in thing:
-    #                     parenthesis_in_progess = True
-    #                 if ")" in thing and parenthesis_in_progress:
-    #                     parenthesis_in_progress = False
-    #
-    #                 if not parenthesis_in_progress:
-    #                     if not isinstance(thing, str):
-    #                         try:
-    #                             n = thing['href']
-    #                             if not n in visited:
-    #                                 next_url_found = True
-    #                                 nexturl = n
-    #                                 visited.append(nexturl)
-    #                         except:
-    #                             pass
-    #
-    # nexturl = "http://en.wikipedia.org" + nexturl
-    # # get list of all unvisited possible urls
-    # out_urls = [url.get('href') for url in soup.findAll('a') if
-    #             (is_valid_url(url.get('href')) and
-    #              url.get('href') not in visited_links)]
-    # # if we have no possible urls to visit, break
-    # # else add
-    # if len(out_urls) == 0:
-    #     print("Could not find Philosophy, out urls was empty! Breaking")
-    #     break
-    # else:
-    #     # print(out_urls[0])
-    #     title = out_urls[0]
-    #     visited_links[title] = True
