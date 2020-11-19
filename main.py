83a5afbf67c561531e6800a7df0b476f549d8b35
Blake Williams <michaelbwilliams100@gmail.com>
2020-11-18 22:21:24 -0500

    When page with no links, go back to previous page

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def is_valid_url(url):
 
 def crawl(next_link):
     next_link = "/wiki/" + next_link
+    curr_link = ""
     visited_links = []
 
     # if title is not form /wiki/validTitle break
@@ -18,53 +19,60 @@ def crawl(next_link):
         exit()
 
     while True:
-        next_link = "https://en.wikipedia.org" + next_link
+        # print("current: " + curr_link[24:])
+        # print("next: " + next_link)
+        if curr_link[24:] == next_link:
+            curr_link = visited_links[-2]
+            # print("vis link -2 : " + visited_links[-2])
+        else:
+            curr_link = next_link
+        curr_link = "https://en.wikipedia.org" + curr_link
         try:
-            res = requests.get(next_link)
+            res = requests.get(curr_link)
+            visited_links.append(curr_link[24:])
+            content = res.content
+
+            soup = BeautifulSoup(content, 'html.parser')
+            # get the title from current header
+            title = soup.find(id="firstHeading")
+            # print(title)
+
+            print(title.string)
+            if title.string == "Philosophy":
+                print("You crawled through {} pages to Philosophy!\n".format(len(visited_links)))
+                return
+
+            # paragraphs = soup.find(id="bodyContent").findAll("p")
+            # paragraphs = content.findAll('p')
+
+            found_next_url = False
+            for p in soup.find(id="bodyContent").find_all("p"):
+                # avoiding <p> tags nested within other elements, such as tables
+                if not found_next_url or p.parent.id != "bodyContent":
+                    parenthesis = False
+                    for text in p.contents:
+                        if not found_next_url:
+                            # print("Text line 62: " + str(text))
+                            if "(" in text:
+                                parenthesis = True
+                            if ")" in text and parenthesis:
+                                parenthesis = False
+
+                            if not parenthesis:
+                                if not isinstance(text, str):
+                                    try:
+                                        link = text['href']
+                                        # print("X: " + x)
+                                        if link not in visited_links and "." not in link:
+                                            found_next_url = True
+                                            # print(x)
+                                            next_link = link
+                                            # visited_links.append(next_link)
+                                    except:
+                                        pass
         except requests.ConnectionError as e:
-            print("Invalid Wikipedia URL, " + next_link + " does not exist! Please try again.\n")
+            print("Invalid Wikipedia URL, " + curr_link + " does not exist! Please try again.\n")
             return
-
-        content = res.content
-
-        soup = BeautifulSoup(content, 'html.parser')
-        # get the title from current header
-        title = soup.find(id="firstHeading")
-        # print(title)
-
-        print(title.string)
-        if title.string == "Philosophy":
-            print("You crawled through {} pages to Philosophy!\n".format(len(visited_links)))
-            return
-
-        # paragraphs = soup.find(id="bodyContent").findAll("p")
-        # paragraphs = content.findAll('p')
-
-        found_next_url = False
-        for p in soup.find(id="bodyContent").find_all("p"):
-            # avoiding <p> tags nested within other elements, such as tables
-            if not found_next_url or p.parent.id != "bodyContent":
-                parenthesis = False
-                for text in p.contents:
-                    if not found_next_url:
-                        # print("Text line 62: " + str(text))
-                        if "(" in text:
-                            parenthesis = True
-                        if ")" in text and parenthesis:
-                            parenthesis = False
-
-                        if not parenthesis:
-                            if not isinstance(text, str):
-                                try:
-                                    link = text['href']
-                                    # print("X: " + x)
-                                    if link not in visited_links and "." not in link:
-                                        found_next_url = True
-                                        # print(x)
-                                        next_link = link
-                                        visited_links.append(next_link)
-                                except:
-                                    pass
 
 
 while True:
